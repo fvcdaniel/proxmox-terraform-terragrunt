3ba6ad755988be431d577a75fcb2dfc2f2e2c09c
Daniel Fernandes Vilar Cardoso <daniel.cardoso@ifpb.edu.br>
2024-07-27 23:17:34 -0300

    parameterize node_name

--- proxmox-modules/vm/files.tf
+++ proxmox-modules/vm/files.tf
@@ -1,7 +1,7 @@
 resource "proxmox_virtual_environment_file" "debian_cloud_image" {
   content_type = "iso"
   datastore_id = "local"
-  node_name    = "pve"
+  node_name    = var.node_name
 
   source_file {
     path      = "https://cdimage.debian.org/images/cloud/bookworm/20240429-1732/debian-12-genericcloud-amd64-20240429-1732.qcow2"
@@ -12,7 +12,7 @@ resource "proxmox_virtual_environment_file" "debian_cloud_image" {
 resource "proxmox_virtual_environment_file" "cloud_config" {
   content_type = "snippets"
   datastore_id = "local"
-  node_name    = "pve"
+  node_name    = var.node_name
 
   source_file {
     path = "${path.module}/cloud-init/user-data.yml"
